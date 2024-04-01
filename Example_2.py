2e8ca3c0f28e6eaff807aaf9e385909d34cb4849
gstinoco <gstinoco@gmail.com>
2024-04-01 08:20:20 -0600

    New results computed.

--- Example_2.py
+++ Example_2.py
@@ -85,7 +85,7 @@ for me in sizes:
             tt -= 1
         
         ## Wave Equation in 2D computed on a unstructured cloud of points.
-        u_ap, u_ex, vec = Wave_2D.Cloud(p, f, g, t, c, cho, r, implicit = True, triangulation = False, tt = tt, lam = 0.75)
+        u_ap, u_ex, vec = Wave_2D.Cloud(p, f, g, t, c, cho, r, implicit = True, triangulation = False, tt = tt, lam = 0.5)
         
         ## Error computation.
         er1 = Errors.Cloud(p, vec, u_ap, u_ex)
@@ -157,7 +157,152 @@ for me in sizes:
             tt -= 1
         
         ## Wave Equation in 2D computed on a unstructured cloud of points.
-        u_ap, u_ex, vec = Wave_2D.Cloud(p, f, g, t, c, cho, r, implicit = True, triangulation = False, tt = tt, lam = 0.75)
+        u_ap, u_ex, vec = Wave_2D.Cloud(p, f, g, t, c, cho, r, implicit = True, triangulation = False, tt = tt, lam = 0.5)
+        
+        ## Error computation.
+        er1 = Errors.Cloud(p, vec, u_ap, u_ex)
+        print('\tThe mean square error is:\t\t', er1.mean())
+        
+        if Save:
+            mdic = {'u_ap': u_ap, 'p': p, 'tt': tt}
+            if Holes:
+                if regular:
+                    folder = 'Results/Example 2/Holes/' + reg
+                else:
+                    folder = 'Results/Example 2/Holes_rand/' + reg
+            else:
+                if regular:
+                    folder = 'Results/Example 2/Clouds/' + reg
+                else:
+                    folder = 'Results/Example 2/Clouds_rand/' + reg
+                
+            if not os.path.exists(folder):
+                os.makedirs(folder)
+            Graph.Cloud(p, tt, u_ap, u_ex, save = True, nom = folder + '/' + reg + '_' + cloud + '.mp4')
+            Graph.Cloud_Steps(p, tt, u_ap, u_ex, nom = folder + '/' + reg + '_' + cloud)
+            #file_n = folder + '/' + reg + '_' + cloud + '.mat'
+            #savemat(file_n, mdic)
+            #make_tarfile(file_n + '.tar.gz', file_n)
+        else:
+            Graph.Cloud(p, tt, u_ap, u_ex, save = False)
+    
+Holes = False
+regular = True
+
+## Run the example for all the chosen regions.
+for me in sizes:
+    cloud = str(me)
+    if first:
+        # Find all the regions.
+        if Holes:
+            if regular:
+                regions = glob.glob(f'Data/Holes/' + cloud + '/*.mat')
+            else:
+                regions = glob.glob(f'Data/Holes_rand/' + cloud + '/*.mat')
+        else:
+            if regular:
+                regions = glob.glob(f'Data/Clouds/' + cloud + '/*.mat')
+            else:
+                regions = glob.glob(f'Data/Clouds_rand/' + cloud + '/*.mat')
+            
+        regions = sorted([os.path.splitext(os.path.basename(region))[0] for region in regions])
+        first = False
+
+    for reg in regions:
+        print('Region: ' + reg + ', with size: ' + cloud)
+        
+        # All data is loaded from the file
+        if Holes:
+            if regular:
+                mat = loadmat('Data/Holes/' + cloud + '/' + reg + '.mat')
+            else:
+                mat = loadmat('Data/Holes_rand/' + cloud + '/' + reg + '.mat')
+        else:
+            if regular:
+                mat = loadmat('Data/Clouds/' + cloud + '/' + reg + '.mat')
+            else:
+                mat = loadmat('Data/Clouds_rand/' + cloud + '/' + reg + '.mat')
+
+        # Node data is saved
+        p   = mat['p']
+        tt  = mat['tt']
+        if tt.min() == 1:
+            tt -= 1
+        
+        ## Wave Equation in 2D computed on a unstructured cloud of points.
+        u_ap, u_ex, vec = Wave_2D.Cloud(p, f, g, t, c, cho, r, implicit = True, triangulation = False, tt = tt, lam = 0.5)
+        
+        ## Error computation.
+        er1 = Errors.Cloud(p, vec, u_ap, u_ex)
+        print('\tThe mean square error is:\t\t', er1.mean())
+        
+        if Save:
+            mdic = {'u_ap': u_ap, 'p': p, 'tt': tt}
+            if Holes:
+                if regular:
+                    folder = 'Results/Example 2/Holes/' + reg
+                else:
+                    folder = 'Results/Example 2/Holes_rand/' + reg
+            else:
+                if regular:
+                    folder = 'Results/Example 2/Clouds/' + reg
+                else:
+                    folder = 'Results/Example 2/Clouds_rand/' + reg
+                
+            if not os.path.exists(folder):
+                os.makedirs(folder)
+            Graph.Cloud(p, tt, u_ap, u_ex, save = True, nom = folder + '/' + reg + '_' + cloud + '.mp4')
+            Graph.Cloud_Steps(p, tt, u_ap, u_ex, nom = folder + '/' + reg + '_' + cloud)
+            #file_n = folder + '/' + reg + '_' + cloud + '.mat'
+            #savemat(file_n, mdic)
+            #make_tarfile(file_n + '.tar.gz', file_n)
+        else:
+            Graph.Cloud(p, tt, u_ap, u_ex, save = False)
+    
+Holes = True                                                                        # Should I use clouds with holes?
+
+## Run the example for all the chosen regions.
+for me in sizes:
+    cloud = str(me)
+    if first:
+        # Find all the regions.
+        if Holes:
+            if regular:
+                regions = glob.glob(f'Data/Holes/' + cloud + '/*.mat')
+            else:
+                regions = glob.glob(f'Data/Holes_rand/' + cloud + '/*.mat')
+        else:
+            if regular:
+                regions = glob.glob(f'Data/Clouds/' + cloud + '/*.mat')
+            else:
+                regions = glob.glob(f'Data/Clouds_rand/' + cloud + '/*.mat')
+            
+        regions = sorted([os.path.splitext(os.path.basename(region))[0] for region in regions])
+        first = False
+
+    for reg in regions:
+        print('Region: ' + reg + ', with size: ' + cloud)
+        
+        # All data is loaded from the file
+        if Holes:
+            if regular:
+                mat = loadmat('Data/Holes/' + cloud + '/' + reg + '.mat')
+            else:
+                mat = loadmat('Data/Holes_rand/' + cloud + '/' + reg + '.mat')
+        else:
+            if regular:
+                mat = loadmat('Data/Clouds/' + cloud + '/' + reg + '.mat')
+            else:
+                mat = loadmat('Data/Clouds_rand/' + cloud + '/' + reg + '.mat')
+
+        # Node data is saved
+        p   = mat['p']
+        tt  = mat['tt']
+        if tt.min() == 1:
+            tt -= 1
+        
+        ## Wave Equation in 2D computed on a unstructured cloud of points.
+        u_ap, u_ex, vec = Wave_2D.Cloud(p, f, g, t, c, cho, r, implicit = True, triangulation = False, tt = tt, lam = 0.5)
         
         ## Error computation.
         er1 = Errors.Cloud(p, vec, u_ap, u_ex)
